2bf1c36d8b7b0c0c638c2e6f27ca45925f87972d
DylanWiggill <wiggilldylan@gmail.com>
2022-03-15 10:44:00 +1300

    Dylans Work

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import './styles/index.scss'
 import DashBoard from './DashBoard';
 import DashBoardMapped from './DashBoardMapped'
+import Dexie from 'dexie';
 
 function App() {
   return (
     <div className="App">
       <div className='dashboard-container'>
         <DashBoardMapped />
-        <DashBoard /> 
       </div>
     </div>
   );
--- src/DashBoard.jsx
+++ src/DashBoard.jsx
@@ -1,13 +1,28 @@
 import React from "react";
+import { sprintData } from "./sprintData";
+import {db} from "./db";
+import {useLiveQuery} from "dexie-react-hooks";
+
+
+
+
 
 function DashBoard () {
 
+  const sprintData = useLiveQuery(() => db.sprintData.toArray())
+
   return (
     <>
-      <div className='sprint-box'>
-        <h1>Sprint Name Goes Here</h1>
-        <p>Sprint Blurb Goes Here</p>
-      </div>
+
+        {sprintData?.map( sprint => {
+          return( 
+            <div key={sprint.id} className = 'sprint-box'>
+              <h1>{sprint.name}</h1>
+              <p>{sprint.blurb}</p>
+            </div>
+          )
+        })}
+      
     </>
   )
 }
